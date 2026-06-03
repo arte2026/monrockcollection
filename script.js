ca0a313a1fcdee46af7bcf0fe016c8cbe4902dac
Guillermo Rayas M <bodisatva5@outlook.com>
2026-06-02 22:12:55 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,20 +9,3 @@ if (isMobile) {
 window.addEventListener('DOMContentLoaded', () => {
     filterArt('hollywood');
 });
-
- //WELCOME
-const welcomeScreen = document.getElementById("welcomeScreen");
-const enterBtn = document.getElementById("enterBtn");
-
-enterBtn.addEventListener("click", () => {
-  welcomeScreen.classList.add("fade-out");
-
-  setTimeout(() => {
-    welcomeScreen.style.display = "none";
-  }, 900);
-
-});
-welcomeScreen.addEventListener("click", () => {
-  enterBtn.click();
-});
-
